b0cb7cfc14d2353f19055c021028b08de4e4a32a
Guido Pleßmann <guido.plessmann@rl-institut.de>
2019-10-03 22:35:10 +0200

    Backgroundmap in function + MV grid district number from file

--- js/backgroundmap.js
+++ js/backgroundmap.js
@@ -1,12 +1,14 @@
 // Create background map with Leaftlet
-  var map = L.map('map');
+function backgroundmap (mv_grid_district_id) {
+  var map = L.map('map').setView([47, 2], 5);
+
   L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
       subdomains: 'abcd'
   }).addTo(map);
 
   // use mv grid district bounds to reset background map
-  polygon = "data/mv_grid_district_2659.geojson"
+  polygon = "data/mv_grid_district_" + mv_grid_district_id + ".geojson"
   d3.json(polygon, function(geoShape) {
 
     var b = d3.geoBounds(geoShape);
@@ -16,3 +18,5 @@
     // L.rectangle(bounds, {color: "#ff7800", weight: 1}).addTo(map);
     map.fitBounds(bounds);
   });
+  return map
+}
